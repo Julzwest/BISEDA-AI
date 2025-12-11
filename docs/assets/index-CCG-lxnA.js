13e78f95bf62179d14dfbb0d0f201fc1a1e6d0c7
Julzwest <yxjgvsrv26@privaterelay.appleid.com>
2025-12-11 02:06:27 +0000

    🚀 Rebuild with grid layout home

--- docs/assets/index-CCG-lxnA.js
+++
@@ -1,8 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-LPMnIfmU.js","assets/index-D6rfMhNR.js","assets/index-BiGDChHV.css"])))=>i.map(i=>d[i]);
-import { r as registerPlugin, _ as __vitePreload } from "./index-D6rfMhNR.js";
-const SignInWithApple = registerPlugin("SignInWithApple", {
-  web: () => __vitePreload(() => import("./web-LPMnIfmU.js"), true ? __vite__mapDeps([0,1,2]) : void 0).then((m) => new m.SignInWithAppleWeb())
-});
-export {
-  SignInWithApple
-};
--- docs/assets/web-daVwm3e4.js
+++ docs/assets/web-daVwm3e4.js
@@ -1,4 +1,4 @@
-import { W as WebPlugin } from "./index-D6rfMhNR.js";
+import { W as WebPlugin } from "./index-D11VyP3e.js";
 var script = { exports: {} };
 var hasRequiredScript;
 function requireScript() {
