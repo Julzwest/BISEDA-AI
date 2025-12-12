4650cec3c3a0997f7543c65161ee462b2aefb774
Julzwest <yxjgvsrv26@privaterelay.appleid.com>
2025-12-12 23:20:16 +0000

    💡 Add screenshot upload tip to AI Coach welcome message
    Users now see a helpful tip explaining they can:
    - Upload screenshots from WhatsApp
    - Upload from Tinder, Bumble, Instagram DMs
    - Get help crafting the perfect reply
    
    Updated in both English and Albanian

--- docs/assets/web-CsL6Timu.js
+++ docs/assets/web-CsL6Timu.js
@@ -1,4 +1,4 @@
-import { W as WebPlugin } from "./index-8Ur7DmND.js";
+import { W as WebPlugin } from "./index-DRZC1-HA.js";
 var script = { exports: {} };
 var hasRequiredScript;
 function requireScript() {
