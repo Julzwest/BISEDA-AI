cc6633ef6ee50b13fc60c642dd021869fed8a001
Julzwest <yxjgvsrv26@privaterelay.appleid.com>
2025-12-14 22:05:50 +0000

    Replace app with landing page

--- docs/assets/index-CPIhGNy3.js
+++
@@ -1,8 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BThR3qIA.js","assets/index-DAUyOH6M.js","assets/index-YvLGjp8z.css"])))=>i.map(i=>d[i]);
-import { r as registerPlugin, _ as __vitePreload } from "./index-DAUyOH6M.js";
-const SignInWithApple = registerPlugin("SignInWithApple", {
-  web: () => __vitePreload(() => import("./web-BThR3qIA.js"), true ? __vite__mapDeps([0,1,2]) : void 0).then((m) => new m.SignInWithAppleWeb())
-});
-export {
-  SignInWithApple
-};
